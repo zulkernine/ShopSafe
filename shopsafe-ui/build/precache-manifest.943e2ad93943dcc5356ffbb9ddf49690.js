3ad503993feafcb0eea50bba40052c054ecdcfdb
zulkernine <mohiuddinmondal107@gmail.com>
2021-07-19 03:16:12 +0530

    improved

---
+++ shopsafe-ui/build/precache-manifest.943e2ad93943dcc5356ffbb9ddf49690.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "3227748279e2678a6e1b",
+    "url": "/static/css/main.e6b0dfc4.chunk.css"
+  },
+  {
+    "revision": "3227748279e2678a6e1b",
+    "url": "/static/js/main.b5ddc7fa.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "b2e6e4b6ac8e81bf432e",
+    "url": "/static/css/2.c33add5a.chunk.css"
+  },
+  {
+    "revision": "b2e6e4b6ac8e81bf432e",
+    "url": "/static/js/2.b6cfe5a8.chunk.js"
+  },
+  {
+    "revision": "d55716a71d86359134254bd37a6828e2",
+    "url": "/index.html"
+  }
+];
